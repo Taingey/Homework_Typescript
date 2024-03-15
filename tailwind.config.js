44c1867cac6ff8285cc0baad1c15991a8aa61770
Taingey <116492233+Taingey@users.noreply.github.com>
2024-03-15 22:03:32 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,26 +9,6 @@ module.exports = {
       container: {
         padding : "3rem",
       },
-      fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
-        Bebas_Neue: ["Bebas Neue"],
-        Vollkorn: ["Vollkorn"],
-        Satisfy: ["Satisfy"],
-        Staatliches: ["Staatliches"]
-      },
-      boxShadow: {
-        shadowHover: [
-          "rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.2) 0px 18px 36px -18px inset"
-        ],
-        shadowStripe: [
-          "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;"
-        ],
-        shadowRegion: ["0px 14px 5px 1px rgb(206, 220, 221)"]
-      },
-      backgroundColor: {
-        darkGray: "#303136",
-        darkBlue: "#1f2937"
-      }
     },
     screens: {
       md: { max: "1050px" },
